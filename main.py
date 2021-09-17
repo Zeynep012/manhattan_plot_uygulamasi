b46add2b4631d393c80a2b3545bd567db65ebeeb
Zeynep Demirtaş <55841375+Zeynep012@users.noreply.github.com>
2021-09-17 02:50:03 +0200

    Hata yakalama işlemleri eklendi

--- main.py
+++ main.py
@@ -12,45 +12,82 @@ class YuklemePenceresi(QMainWindow, YuklemeArayuz):
     def __init__(self):
         super().__init__()
         self.DosyaYolu = ''
+        self.uygulama_kulavuzu = "Manhattan grafiğini çizdirmek için önce verileriniz olduğu dosyayı seçiniz. Daha sonra eşik değeri girip 'MANHATTAN PLOT'  butonuna tıklayınız."
         self.basla()
     
     def basla(self):
-        self.setupUi(self)
-        self.pb_grafik_cizimi.setValue(0)
-        self.Aktar.clicked.connect(self.AktarFonk)
-        self.manPlot.clicked.connect(self.grafik_cizdir)
+        try:
+            self.setupUi(self)
+            self.pb_grafik_cizimi.setValue(0)
+            self.Aktar.clicked.connect(self.AktarFonk)
+            self.manPlot.clicked.connect(self.grafik_cizdir)
+        except Exception as hata :
+            self.uyari_kutusu.setText(hata)
        
     def DosyaSec(self):
-        self.dosya_iletisim = QFileDialog(self)
-        self.dosya_ac = self.dosya_iletisim.getOpenFileName(self,"Dosya Ac")
-        return self.dosya_ac[0]
+        try:
+            self.dosya_iletisim = QFileDialog(self)
+            self.dosya_ac = self.dosya_iletisim.getOpenFileName(self,"Dosya Ac")
+            return self.dosya_ac[0]
+        except Exception as hata :
+            self.uyari_kutusu.setText(hata)
+    
+    def uzanti_bul(self,dosya):
+        try:
+            k = 0
+            kntrl = False
+            uzanti = []
+            for i,parca in enumerate(dosya):
+            #bu dosya yolunun sonundaki dosyanin uzantisini ayiran dongu
+                if parca == '.' or kntrl == True:
+                    kntrl = True
+                    uzanti.insert(k,dosya[i])
+                    k += 1
+            return uzanti
+        except Exception as hata :
+            self.uyari_kutusu.setText(hata)
     
     def AktarFonk(self):
-        self.DosyaYolu = self.DosyaSec()
-        
+        try:
+            self.DosyaYolu = self.DosyaSec()
+            if self.DosyaYolu == "":
+                self.uyari_kutusu.setText("DOSYA SEÇİMİ YAPILMADI veya BİR HATA YAŞANDI! \n" + self.uygulama_kulavuzu)
+            elif self.uzanti_bul(self.DosyaYolu) != ['.', 'x', 'l', 's', 'x']:
+                self.uyari_kutusu.setText("SEÇTİĞİNİZ DOSYA 'xlsx' UZANTILI OLMALIDIR! \n" + self.uygulama_kulavuzu)
+            else:
+                self.uyari_kutusu.setText("Şimdi eşik değerini girip grafiği çizdirebilirsiniz.")
+        except Exception as hata :
+            self.uyari_kutusu.setText(hata)
+            
     def yukleniyor(self,ilerleme_degeri):
-        self.pb_grafik_cizimi.setValue(ilerleme_degeri)
+        try:
+            self.pb_grafik_cizimi.setValue(ilerleme_degeri)
+        except Exception as hata :
+            self.uyari_kutusu.setText(hata)
         
     def grafik_cizdir(self):
-        self.uyari_kutusu.setText("Seçilen dosyadaki veriler çekiliyor. Lütfen bu işlemler yapılırken bilgisayarda yeni bir işlem başlatmayınız!")
-        girilen_esik_deger = self.EsikDegeri.value()
-        dosya_yolu  = r'%s' % self.DosyaYolu
-        self.yukleniyor(5)
-        grf.veri_to_df(dosya_yolu)
-        self.yukleniyor(15)
-        grf.kumulatif_hesap()
-        self.yukleniyor(35)
-        grf.eks_log_hesabi()
-        self.yukleniyor(50)
-        grf.birlestirme_islemi()
-        self.uyari_kutusu.setText("...")
-        self.yukleniyor(70)
-        grf.grafik_ciz(girilen_esik_deger)
-        self.uyari_kutusu.setText("Belirlediğiniz eşik değerin üstündeki veriler aranıyor. Bu adım veri sayısına bağlı olarak uzun sürebilir.")
-        self.yukleniyor(90)
-        grf.esik_deger_tarama(girilen_esik_deger)
-        self.uyari_kutusu.setText("Manhattan Plot çizildi.")
-        self.yukleniyor(100)
+        try:
+            self.uyari_kutusu.setText("Seçilen dosyadaki veriler çekiliyor. Lütfen bu işlemler yapılırken bilgisayarda yeni bir işlem başlatmayınız!")
+            girilen_esik_deger = self.EsikDegeri.value()
+            dosya_yolu  = r'%s' % self.DosyaYolu
+            self.yukleniyor(5)
+            grf.veri_to_df(dosya_yolu)
+            self.yukleniyor(15)
+            grf.kumulatif_hesap()
+            self.yukleniyor(35)
+            grf.eks_log_hesabi()
+            self.yukleniyor(50)
+            grf.birlestirme_islemi()
+            self.uyari_kutusu.setText("...")
+            self.yukleniyor(70)
+            grf.grafik_ciz(girilen_esik_deger)
+            self.uyari_kutusu.setText("Belirlediğiniz eşik değerin üstündeki veriler aranıyor. Bu adım veri sayısına bağlı olarak uzun sürebilir.")
+            self.yukleniyor(90)
+            grf.esik_deger_tarama(girilen_esik_deger)
+            self.uyari_kutusu.setText("Manhattan Plot çizildi.")
+            self.yukleniyor(100)
+        except Exception as hata :
+            self.uyari_kutusu.setText(hata)
                      
 Uygulama = QApplication(sys.argv)
 YuklemePenceresiNesnesi = YuklemePenceresi()
